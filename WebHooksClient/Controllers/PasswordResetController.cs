870d2be359d97b2f5aac3c522322f5b6fd131e3e
jorocksolid <jo.stevens@rocksolid.com>
2018-11-02 10:26:30 +0000

    Initial Commit

---
+++ WebHooksClient/Controllers/PasswordResetController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebHooksClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordResetController : ControllerBase
+    {
+
+        [Authorize("webhook")]
+        [HttpPost]
+        public IActionResult PasswordReset([FromBody]PasswordResetDTO dto)
+        {
+            if (string.IsNullOrEmpty(dto.email))
+            {
+                return BadRequest("Invalid email");
+            }
+
+            return Ok();
+        }
+
+    }
+
+    public class PasswordResetDTO
+    {
+        public string email;
+    }
+
+    
+}
---
+++ WebHooksClient/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebHooksClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5003";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "admin_ui_webhooks";
+
+                });
+
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("webhook", builder =>
+                {
+                    builder.AddAuthenticationSchemes("Bearer");
+                    builder.RequireScope("admin_ui_webhooks");
+                });
+            });
+
+     }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+
+        }
+    }
+}
